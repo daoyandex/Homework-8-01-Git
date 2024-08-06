43b19501a3e0c51c9b1c1df4039411711a8c7a33
Aleksandr Alekseev <alekseeff_av@mail.ru>
2024-08-07 02:51:04 +0300

    File test.sh has been modified in the conflict branch

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 echo "This is new file named test.sh"
+echo "Fourth String added from just created conflict branch"
 echo "This is third string between first and second string here"
 echo "This is second string added here"
 
