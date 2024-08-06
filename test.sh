382b5c4e3b8264d0bbdd669adfc0a852f40f4dd2
Aleksandr Alekseev <alekseeff_av@mail.ru>
2024-08-07 02:56:36 +0300

    File test.sh has beem modified in the same string like at the conflict branch

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 echo "This is new file named test.sh"
+echo "This is fourth string added in the main branch"
 echo "This is third string between first and second string here"
 echo "This is second string added here"
 
